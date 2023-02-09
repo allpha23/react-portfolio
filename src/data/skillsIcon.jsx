586b7ef6498727f01d87cf785104e20ef0b34543
allpha23 <allpha23@gmail.com>
2023-02-09 12:29:28 -0300

    feat: skills data

---
+++ src/data/skillsIcon.jsx
@@ -0,0 +1,75 @@
+const skillsIcon = [
+  {
+    id: 1,
+    icon: 'akar-icons:react-fill',
+    title: 'ReactJS',
+  },
+  {
+    id: 2,
+    icon: 'cib:javascript',
+    title: 'JavaScript',
+  },
+  {
+    id: 3,
+    icon: 'akar-icons:sass-fill',
+    title: 'Sass',
+  },
+  {
+    id: 4,
+    icon: 'fontisto:css3',
+    title: 'CSS',
+  },
+  {
+    id: 5,
+    icon: 'akar-icons:bootstrap-fill',
+    title: 'Bootstrap',
+  },
+  {
+    id: 6,
+    icon: 'icomoon-free:html-five',
+    title: 'HTML',
+  },
+  {
+    id: 7,
+    icon: 'akar-icons:redux-fill',
+    title: 'Redux',
+  },
+  {
+    id: 8,
+    icon: 'bi:git',
+    title: 'Git',
+  },
+  {
+    id: 9,
+    icon: 'fontisto:github',
+    title: 'GitHub',
+  },
+
+  {
+    id: 10,
+    icon: 'simple-icons:jest',
+    title: 'Jest',
+  },
+  {
+    id: 11,
+    icon: 'fontisto:mysql',
+    title: 'MySQL',
+  },
+  {
+    id: 12,
+    icon: 'file-icons:docker',
+    title: 'Docker',
+  },
+  {
+    id: 13,
+    icon: 'bxl:nodejs',
+    title: 'NodeJS',
+  },
+  {
+    id: 14,
+    icon: 'cib:typescript',
+    title: 'Typescript',
+  },
+];
+
+export default skillsIcon;
