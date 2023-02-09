898356721eb73a341871fd5f8087f72cb7890230
allpha23 <allpha23@gmail.com>
2023-02-09 12:38:38 -0300

    feat(App): adiciona componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <NavBar />
+      <Home />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
